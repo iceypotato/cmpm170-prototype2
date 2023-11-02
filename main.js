4da21e0a176940dfad5360ff2d4ad956c1bad999
Samuel Zhang <sashzhan@ucsc.edu>
2023-11-02 14:53:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-title = "";
+title = "Not Suika Game";
 
-description = ``;
+description = `Color \n shapes \n combiner`;
 
 characters = [];
 
-options = {};
+options = {
+    theme: "shapeDark",
+};
+
+let player;
+let shapes;
 
 function update() {
-  if (!ticks) {
-  }
+    if (!ticks) {
+        init()
+    };
+
+    if (input.isJustPressed) {
+        
+        console.log("Ball Dropped");
+    }
+    color("cyan")
+    box(player.pos, 4)
+
+    //shapes.push({ pos: vec(50 - x, y)});
+    //color("light_cyan");
+    //rect(5, 0, 90, 5);
+}
+
+function generateShape() {
+    
+}
+
+function init() {
+    player = {pos: vec(50, 10), vel: vec()};
+    shapes = [];
 }
 
 addEventListener("load", onLoad);
