1c794706fcf88b8ef98bc9f49a9ad2ebcd43db06
Jentsen <jentsenmaniti@gmail.com>
2023-11-17 15:24:42 -0800

    added absorption and randomized colors

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-title = "Shape Game";
+title = "Not Suika Game";
 
-description = `Color \n Shapes \n Combiner`;
+description = `[Tap] to Drop`;
 
 characters = [];
 
 options = {
   theme: "shapeDark",
   isShowingScore: true,
+  isPlayingBgm: true,
+  isReplayEnabled: true,
+  seed: 3,
 };
 
 /**
@@ -14,89 +17,92 @@ options = {
 * pos: Vector, vel: Vector, radius: number, targetRadius: number,
 * }[]}
 */
-
 let shapes;
-let pos;
-let radius;
-let targetRadius;
-
-function init() { 
-  // Initialize your variables here if needed
-}
 
 function getRandomColor() {
-  const colors = ["blue", "red", "green"];
+  const colors = ["blue", "red", "green", "yellow", "purple"];
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
+function init() {
+  shapes = [];
+}
+
 function update() {
+  // Initialize on the first frame
   if (!ticks) {
     shapes = [];
-    pos = vec();
-    radius = undefined;
   }
 
+  // Create a new shape when the input is just pressed
   if (input.isJustPressed) {
     const newShape = {
-      pos: vec(input.pos.x, input.pos.y),
-      vel: vec(0, 0.5), // Add velocity to make it fall
+      pos: vec(input.pos.x, 15),
+      vel: vec(0, 0.5),
       radius: 0,
       targetRadius: Math.random() * 10 + 2,
-      
+      color: getRandomColor(), // Generate color here
     };
     shapes.push(newShape);
   }
 
   // Update and draw the shapes
   remove(shapes, (shape) => {
-    shape.radius += (shape.targetRadius - shape.radius) * 0.05;
-    shape.vel.y += 0.05; // Apply gravity
+    shape.radius += (shape.targetRadius - shape.radius) * 0.025;
+    shape.vel.y += 0.05;
 
     shape.pos.add(shape.vel);
 
-    shapes.forEach((otherShape) => {
+    shapes.forEach((otherShape, index) => {
       if (shape !== otherShape) {
         const d = shape.pos.distanceTo(otherShape.pos) - shape.radius - otherShape.radius;
         if (d < 0) {
           const angle = otherShape.pos.angleTo(shape.pos);
           const overlap = shape.radius + otherShape.radius - shape.pos.distanceTo(otherShape.pos);
-          
-          // Adjust the position to prevent sticking
+
+          // Adjust positions so that the shapes don't overlap
           const moveBy = overlap / 2;
           shape.pos.addWithAngle(angle, moveBy);
           otherShape.pos.addWithAngle(angle + PI, moveBy);
-          
-          // Adjust the velocity for bounce effect
+
+          // Adjust velocities to bounce off each other
           const averageBounce = (shape.vel.y + otherShape.vel.y) / 2;
           shape.vel.y = averageBounce;
           otherShape.vel.y = averageBounce;
+
+          // Check if shapes have the same color
+          if (shape.color === otherShape.color) {
+            // Combine shapes by adding radii
+            shape.targetRadius += otherShape.radius;
+            console.log(`Circles of ${shape.color} and ${otherShape.color} are touching. Combined radius: ${shape.targetRadius}`);
+           
+            //increase score based on size of shape created
+            addScore(shape.targetRadius, shape.pos);
+
+            // Remove the other shape
+            shapes.splice(index, 1);
+          }
         }
       }
     });
-  
 
-    // Draw the shape, can call generateShape() here for the real branch
-    const randomColor = getRandomColor();
-    color('red');
+    // Draw the shape with the pre-generated color
+    color(shape.color);
     arc(shape.pos, shape.radius, 2);
-    
 
-    // Stop shape from rolling off the sides of the screen
-    if (shape.pos.x - shape.radius < 0){
+    // Stop shape from going off the sides
+    if (shape.pos.x - shape.radius < 0 || shape.pos.x + shape.radius > 99) {
       shape.vel.x = 0;
-      shape.pos.x = shape.radius;
-    }
-    else if (shape.pos.x + shape.radius > 99){
-      shape.vel.x = 0;
-      shape.pos.x = 99 - shape.radius;
+      shape.pos.x = clamp(shape.pos.x, shape.radius, 99 - shape.radius);
     }
 
-    // Stop shape when it hits the bot of the screen
+    // Stop shape when it hits the bottom
     if (shape.pos.y + shape.radius >= 99) {
-      shape.vel.y = 0; // Stop the vertical velocity
-      shape.pos.y = 99 - shape.radius; // Adjust the position to be just above the bottom
+      shape.vel.y = 0;
+      shape.pos.y = 99 - shape.radius;
     }
 
-    return shape.radius >= shape.targetRadius; // Remove when it reaches the target radius
+    // Remove when it reaches the target radius
+    return shape.radius >= shape.targetRadius;
   });
 }
